7f586a0882d147033c820362118e3682b906fb9c
francisco-leal <francisco@talentprotocol.com>
2025-01-12 13:16:42 +0000

    Increase permissions from github and store auth token on session

--- config/auth-options.ts
+++ config/auth-options.ts
@@ -1,11 +1,31 @@
 import GithubProvider from "next-auth/providers/github";
+import { NextAuthOptions } from "next-auth";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+      authorization: {
+        params: {
+          scope: "read:user repo", // Request read access to user data and repositories
+        },
+      },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET!,
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      // Send the access_token to the client
+      session.accessToken = token.accessToken as string;
+      return session;
+    },
+  },
 };
